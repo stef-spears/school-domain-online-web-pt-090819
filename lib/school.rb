7ad3fbbe715573d0cf92e59b6dfc96e36777ae7e
Stef Spears <spears.sj87@gmail.com>
2019-09-27 23:33:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  
+  def add_student(student_name, grade)
+    if roster[grade]
+     roster[grade] << student_name
+   else roster[grade] = []
+     roster[grade] << student_name
+    end
+  end 
+  
+  def grade(grade)
+    roster[grade]
+  end 
+  
+  def sort 
+    
+  end
+  
+end 
